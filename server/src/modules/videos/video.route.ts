e28558ba5028b6bd64d09e6064fdaf4b296b0cf1
Roman Trynko <trynkoroman25@gmail.com>
2022-09-06 09:40:02 +0300

    added videoUploader

--- server/src/modules/videos/video.route.ts
+++ server/src/modules/videos/video.route.ts
@@ -1,4 +1,8 @@
 import express from 'express';
+import requireUser from '../../middleware/requireUser';
+import { uploadVideoHandler } from './video.controller';
 const router = express.Router();
+
+router.post('/', requireUser, uploadVideoHandler);
 
 export default router;
